a30900a370816e694f279550b3262584313204ac
NightFox <antonycoll123@gmail.com>
2024-03-08 22:53:47 -0600

    Practica4
    aaaa

--- Tienda/src/main/java/com/tienda/Tienda/domain/producto.java
+++ Tienda/src/main/java/com/tienda/Tienda/domain/producto.java
@@ -4,36 +4,41 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import lombok.Data;
 
-@Data // Tipo Data 
-@Entity //Identidad
-@Table(name = "producto") //Nombre de la tabla
-
-public class producto implements Serializable { 
 
 
-    private static final long serialVersionUID = 1L; 
+@Data
+@Entity
+@Table(name = "producto")
+public class producto implements Serializable {
 
-    
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    @Column(name = "id_producto") 
-    private long idproducto; 
-    private long idCategoria;
-    private String descripcion; 
-    private String detalle; 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idproducto;
+
+    @ManyToOne // Relación ManyToOne con Categoria
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+
+    private String descripcion;
+    private String detalle;
     private double precio;
     private int existencias;
     private String rutaImagen;
     private boolean activo;
 
-
+    // Constructor con y sin parametros
     public producto() {
     }
-    
-    public producto(String descripcion, boolean activo) {
+
+    public producto(String descripcion, String detalle, double precio, int existencias, boolean activo) {
         this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
         this.activo = activo;
     }
-
 }
 
